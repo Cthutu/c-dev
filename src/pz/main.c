ac6a3c60fa6b54a2ba6765e6424d412c42e4acd6
Matt Davies <mattie.davies@gmail.com>
2025-12-06 12:43:21 +0000

    feat: Introduce new Pixelf Zombie application

---
+++ src/pz/main.c
@@ -0,0 +1,10 @@
+#define KORE_IMPLEMENTATION
+#include <kore/kore.h>
+
+int kmain(int argc, char** argv) {
+    KORE_UNUSED(argc);
+    KORE_UNUSED(argv);
+
+    prn("Hello, Kore World!");
+    return 0;
+}
