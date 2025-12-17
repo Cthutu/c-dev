e0896d0871f69965bff3f0345dcf4380aa23bc6f
Matt Davies <mattie.davies@gmail.com>
2025-12-17 14:58:02 +0000

    feat: Introduce unified test runner and framework

---
+++ test/kore/test.c
@@ -0,0 +1,12 @@
+#define TEST_IMPLEMENTATION
+#define KORE_TEST 1
+#define KORE_IMPLEMENTATION
+
+#include <kore/kore.h>
+#include <kore/intern.h>
+#include <kore/string.h>
+#include <test/test.h>
+
+TEST_SUITE_BEGIN()
+RUN_ALL_TESTS();
+TEST_SUITE_END()
